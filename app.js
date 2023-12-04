c53d4db2b6d24c364da6f00f89f7a7f6698cb8cd
Hamisi <97879535+Hamissan@users.noreply.github.com>
2023-12-04 12:33:15 +0300

    Remove .env from tracking

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 // app.js
+
 require('dotenv').config();
 const express = require("express");
-const bodyParser= require("body-parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy= require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
-// const postModule = require('./views/posts.ejs'); // Adjust the path accordingly
-
+const flash = require("express-flash");
 
 const app = express();
 
@@ -18,282 +16,275 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(session({
-  secret:process.env.SECRET_KEY,
+  secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false
 }));
-// initialize passport
-
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-const mongoURL= "mongodb://localhost:27017/userDB";
+const mongoURL = "mongodb://localhost:27017/userDB";
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
 
-mongoose.connect(mongoURL,mongoOptions)
-.then(() => {
+mongoose.connect(mongoURL, mongoOptions)
+  .then(() => {
     console.log('Connected to MongoDB');
-})
-.catch((error) => {
-    console.log("Error connecting to -MongoDB.", error.message);
-});
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB.", error.message);
+  });
 
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String,
-// store details from google  
-googleId: String,
-secret: String,
-displayName: String,
-email: String,
-profileName: String,
-// 
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
 });
 
 userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      cb(null, { id: user.id, username: user.username, name: user.name });
-    });
-  });
-  
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-  });
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/Secret", 
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log("Google authentication Successful");
-    console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
     });
-  }
-));
+});
 
 const postSchema = new mongoose.Schema({
-    title: String,
-    content: String,  // Add this line
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    date: { type: Date, default: Date.now }
-  });
+  title: String,
+  content: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  date: { type: Date, default: Date.now }
+});
 
-  const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
-    res.render("home", { currentUser: req.user }); // Assuming you have user information stored in req.user
+  res.render("home", { currentUser: req.user });
 });
 
-app.get("/auth/google",passport.authenticate("google",{scope:["profile"]}));
+app.get("/register", function (req, res) {
+  res.render("register", { currentUser: req.user });
+});
 
- // the above line of code is sufficient enough to bring a pop up to the user tosign in using google
- app.get("/auth/google/Secret", 
- passport.authenticate('google', { failureRedirect: '/' }),
- function(req, res) {
-   // Successful authentication, redirect home.
-   res.redirect('/posts');
- });
+app.post("/register", async function (req, res) {
+  try {
+    const user = await User.register(new User({ username: req.body.username }), req.body.password);
+    passport.authenticate("local")(req, res, function () {
+      req.flash("success", "Registration successful!");
+      res.redirect("/posts");
+    });
+  } catch (err) {
+    console.error(err);
+    req.flash("error", err.message);
+    res.redirect("/register");
+  }
+});
 
-  
-  app.get("/register", function (req, res) {
-    res.render("register", { currentUser: req, res});
-  });
-  
-  app.post("/register", function (req, res) {
-    User.register({ username: req.body.username }, req.body.password, function (err, user) {
-      if (err) {
-        console.log(err);
-        res.redirect("/register");
-      } else {
-        passport.authenticate("local")(req, res, function () {
-          res.redirect("/posts");
-        });
-      }
-    });
-  });
-  
-  app.get("/login", function (req, res) {
-    res.render("login", {currentUser: req, res});
-  });
-  
-  app.post("/login", function (req, res) {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
-  
-    req.login(user, function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        passport.authenticate("local")(req, res, function () {
-          res.redirect("/posts");
-        });
-      }
-    });
-  });
-  
-  app.get("/logout", function (req, res) {
-    // Example with a callback function
-req.logout(function(err) {
+app.get("/login", function (req, res) {
+  res.render("login", { currentUser: req.user });
+});
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/posts",
+  failureRedirect: "/login",
+  failureFlash: true,
+}));
+
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
     if (err) {
-        return next(err);
+      return next(err);
     }
-    // Additional logic after logout
-    res.redirect('/'); // Redirect to a specific page after logout
+    req.flash("success", "Logout successful!");
+    res.redirect('/');
+  });
 });
 
-  });
-  
-  app.get("/submit", function (req, res) {
-    if (req.isAuthenticated()) {
-      res.render("submit", { currentUser: req.user});
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  app.post("/submit", function (req, res) {
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit", { currentUser: req.user });
+  } else {
+    req.flash("error", "Please login to submit a post.");
+    res.redirect("/login");
+  }
+});
 
-    if (req.isAuthenticated() && req.user) {
-      console.log(req.user);
-      const newPost = new Post({
-        title: req.body.title,
-        content: req.body.content,
-        author: req.user._id,
+app.post("/submit", function (req, res) {
+  if (req.isAuthenticated() && req.user) {
+    const newPost = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.user._id,
+    
+    }); 
+
+    newPost.save()
+      .then(() => {
+        req.user.posts = req.user.posts || [];
+        req.user.posts.push(newPost);
+        return req.user.posts;
+      })
+      .then(() => {
+        req.flash("success", "Post submitted successfully!");
+        res.redirect("/posts");
+      })
+      .catch(err => {
+        console.error(err);
+        req.flash("error", "Error submitting post.");
+        res.redirect("/submit");
       });
-  
-      newPost.save()
-        .then(() => {
-          req.user.posts = req.user.posts || [];
-          req.user.posts.push(newPost);
-          return req.user.save();
-        })
-        .then(() => res.redirect("/posts"))
-        .catch(err => {
-          console.error(err);
-          res.redirect("/submit");
-        });
+  } else {
+    req.flash("error", "Please login to submit a post.");
+    res.redirect("/login");
+  }
+});
+
+app.get("/posts", async function (req, res) {
+  try {
+    if (req.isAuthenticated()) {
+      const posts = await Post.find({ author: req.user._id });
+      res.render("posts", { posts, currentUser: req.user });
     } else {
+      req.flash("error", "Please login to view posts.");
       res.redirect("/login");
     }
-  });
-  app.get("/posts", async function (req, res) {
+  } catch (err) {
+    console.error(err);
+    req.flash("error", "Error fetching posts.");
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Additional routes for editing and deleting posts...
+// ...
+
+// EDIT POST
+app.get("/edit/:postId", async function (req, res) {
+  try {
+    if (req.isAuthenticated()) {
+      const postId = req.params.postId;
+
+      // Find the post by ID using Promises
+      const post = await Post.findById(postId).exec();
+
+      // Check if the post exists and if the author is the authenticated user
+      if (post && req.user && post.author && post.author.equals(req.user._id)) {
+        res.render("edit", { post: post, currentUser: req.user });
+      } else {
+        req.flash("error", "Post not found or you are not the author.");
+        res.redirect("/posts");
+      }
+    } else {
+      req.flash("error", "Please login to edit posts.");
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.error(err);
+    req.flash("error", "Error fetching post details.");
+    res.redirect("/posts");
+  }
+});
+
+app.post("/edit/:postId", async function (req, res) {
+  if (req.isAuthenticated()) {
+    const postId = req.params.postId;
+    const newContent = req.body.content;
+
     try {
-      const posts = await Post.find({});
-      res.render("posts", { posts, currentUser: req.user });
+      const post = await Post.findOne({ _id: postId, author: req.user._id }).exec();
+
+      if (post) {
+        // Update the content of the post
+        post.content = newContent;
+
+        // Save the updated post
+        await post.save();
+
+        req.flash("success", "Post updated successfully!");
+        res.redirect("/posts");
+      } else {
+        req.flash("error", "Post not found or you are not the author.");
+        res.redirect("/posts");
+      }
     } catch (err) {
       console.error(err);
-      res.status(500).send("Internal Server Error");
+      req.flash("error", "Error updating post.");
+      res.redirect("/posts");
     }
-  });
+  } else {
+    req.flash("error", "Please login to edit posts.");
+    res.redirect("/login");
+  }
+});
 
-  app.get("/edit/:postId", async function (req, res) {
-    if (req.isAuthenticated()) {
-      const postId = req.params.postId;
-  
-      try {
-        const post = await Post.findById(postId).exec();
-  
-        if (post && post.author && post.author.equals(req.user._id)) {
-          res.render("edit", { post: post });
-        } else {
-          res.redirect("/posts");
-        }
-      } catch (err) {
-        console.error(err);
-        res.redirect("/posts");
-      }
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  
-  app.post("/edit/:postId", async function (req, res) {
-    if (req.isAuthenticated()) {
-      const postId = req.params.postId;
-      const newContent = req.body.content;
-  
-      try {
-        const post = await Post.findById(postId).exec();
-  
-        if (post && post.author.equals(req.user._id)) {
-          // Update the content of the post
-          post.content = newContent;
-  
-          // Save the updated post
-          await post.save();
-          res.redirect("/posts");
-        } else {
-          res.redirect("/posts");
-        }
-      } catch (err) {
-        console.error(err);
-        res.redirect("/posts");
-      }
-    } else {
-      res.redirect("/login");
-    }
-  });
+// DELETE POST
+app.get("/delete/:postId", async function (req, res) {
+  if (req.isAuthenticated()) {
+    const postId = req.params.postId;
 
-  app.get("/delete/:postId", function (req, res) {
-    if (req.isAuthenticated()) {
-      const postId = req.params.postId;
-  
-      // Find the post by ID and ensure the current user is the author
-      Post.findOneAndDelete({ _id: postId, author: req.user._id }, function (err, post) {
-        if (!err && post) {
-          res.redirect("/posts");
-        } else {
-          console.log(err);
-          res.redirect("/posts");
-        }
-      });
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  app.post("/delete/:postId", async function (req, res) {
-    if (req.isAuthenticated()) {
-      const postId = req.params.postId;
-  
-      try {
-        const post = await Post.findById(postId).exec();
-  
-        if (post && post.author.equals(req.user._id)) {
-          // Remove the post
-          await post.deleteOne();
-          res.redirect("/posts");
-        } else {
-          res.redirect("/posts");
-        }
-      } catch (err) {
-        console.error(err);
+    try {
+      const post = await Post.findOne({ _id: postId, author: req.user._id }).exec();
+
+      if (post) {
+        // Render the confirmation page before actually deleting the post
+        res.render("confirm-delete", { post: post, currentUser: req.user });
+      } else {
+        req.flash("error", "Post not found or you are not the author.");
         res.redirect("/posts");
       }
-    } else {
-      res.redirect("/login");
+    } catch (err) {
+      console.error(err);
+      req.flash("error", "Error fetching post details.");
+      res.redirect("/posts");
     }
-  });
+  } else {
+    req.flash("error", "Please login to delete posts.");
+    res.redirect("/login");
+  }
+});
+
+// POST route for confirming and deleting post
+app.post("/delete/:postId", async function (req, res) {
+  if (req.isAuthenticated()) {
+    const postId = req.params.postId;
+
+    try {
+      const post = await Post.findOneAndDelete({ _id: postId, author: req.user._id }).exec();
+
+      if (post) {
+        req.flash("success", "Post deleted successfully!");
+        res.redirect("/posts");
+      } else {
+        req.flash("error", "Post not found or you are not the author.");
+        res.redirect("/posts");
+      }
+    } catch (err) {
+      console.error(err);
+      req.flash("error", "Error deleting post.");
+      res.redirect("/posts");
+    }
+  } else {
+    req.flash("error", "Please login to delete posts.");
+    res.redirect("/login");
+  }
+});
+
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
